65fc209a725255e3eedb0cb17d4003f9f6d314e8
yamato <yamato1212810@icloud.com>
2024-10-15 20:00:34 +0900

    Initial Commit

---
+++ ITodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ITodo
+//
+//  Created by 本馬大和 on 2024/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ITodo/ITodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITodoApp.swift
+//  ITodo
+//
+//  Created by 本馬大和 on 2024/10/15.
+//
+
+import SwiftUI
+
+@main
+struct ITodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
